a7cda81dfa1f1ad518074600f7f39b417e94e253
rainmaker3363 <jsc22637@gmail.com>
2016-11-24 11:31:02 +0900

    Vampival Week 10_8

---
+++ Assets/Script/UI/Title/MainTitle_BG_Cloude.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainTitle_BG_Cloude : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(new Vector3(0, 0, -10.0f) * Time.deltaTime);
+	}
+}
