afee7689cf479d79a57a256266198da0e5e6218e
rainmaker3363 <jsc22637@gmail.com>
2017-04-17 13:52:10 +0900

    Special Week# 2

--- Assets/Script/UI/Title/Main_Start_Button.cs
+++ Assets/Script/UI/Title/Main_Start_Button.cs
@@ -9,25 +9,44 @@ public class Main_Start_Button : MonoBehaviour, IPointerEnterHandler, ISelectHan
     public AudioClip SceneChange;
     public SoundManager SDManager;
 
+    private bool ButtonChecker;
+
     //public GameObject MissionInfoPanel;
 
     // Use this for initialization
     void Start()
     {
-
+        ButtonChecker = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.anyKeyDown)
+        {
+            if (ButtonChecker == false)
+            {
+                ButtonChecker = true;
+
+                SDManager.PlaySfx(SceneChange);
+
+                AutoFade.LoadLevel("StageLobbyScene", 2.0f, 1.0f, Color.black);
+            }
+        }
 
     }
 
     public void OnSelect(BaseEventData eventData)
     {
-        SDManager.PlaySfx(SceneChange);
+        if(ButtonChecker == false)
+        {
+            ButtonChecker = true;
 
-        AutoFade.LoadLevel("StageLobbyScene", 2.0f, 1.0f, Color.black);
+            SDManager.PlaySfx(SceneChange);
+
+            AutoFade.LoadLevel("StageLobbyScene", 2.0f, 1.0f, Color.black);
+        }
+
         //UnityEngine.SceneManagement.SceneManager.LoadScene("MapTest");
     }
 
